ed5ce6786c7248d717d11accfa5dd45ad55eb07f
Lukas <devmojoo@gmail.com>
2018-10-28 20:56:59 +0200

    Finished github mini app to fetch github user info. TODO: 404 error

---
+++ js/app.js
@@ -0,0 +1,40 @@
+const inputValue = document.querySelector("#search");
+const searchButton = document.querySelector(".searchButton");
+const nameContainer = document.querySelector(".main__profile-name");
+const unContainer = document.querySelector(".main__profile-username");
+const reposContainer = document.querySelector(".main__profile-repos");
+const urlContainer = document.querySelector(".main__profile-url");
+
+const client_id = "";
+const client_secret = "";
+
+
+const fetchUser = async (user) => {
+
+    try {
+        const api_call = await fetch(`https://api.github.com/users/${user}?client_id=${client_id}&client_secret=${client_secret}`);
+        const data = await api_call.json();
+
+        return { data }
+    }
+    catch(e) {
+        nameContainer.innerHTML = `Name: <span class="main__profile-value">Not found</span>`;
+    }
+
+    console.log("fetchUser")
+}
+
+const showData = () => {
+    fetchUser( inputValue.value ).then((res) => {
+            nameContainer.innerHTML = `Name: <span class="main__profile-value">${res.data.name}</span>`;
+            unContainer.innerHTML = `Username: <span class="main__profile-value">${res.data.login}</span>`;
+            reposContainer.innerHTML = `Repos: <span class="main__profile-value">${res.data.public_repos}</span>`;
+            urlContainer.innerHTML = `URL: <span class="main__profile-value">${res.data.html_url}</span>`;
+    }).catch(error => console.log(error));
+}
+
+searchButton.addEventListener("click", () => {
+    if( inputValue.value ) {
+        showData();
+    }
+});
